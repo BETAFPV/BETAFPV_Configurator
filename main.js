dbaa34bd2ba8161001acdd195a6638d52cb662f7
wenzhicode <linkwenzhi@gmail.com>
2021-09-13 09:23:27 +0800

    clean up main.js

--- main.js
+++ main.js
@@ -4,63 +4,7 @@ var electron = require('electron')
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow
 var Menu = electron.Menu
-
 var mainWindow = null
-
-const menuTemplate = [
-    {
-        label:'LiteRadio',
-        submenu:[
-            {
-                label:'Enter to',
-                click(){
-                    mainWindow.loadURL(`file://${__dirname}/liteRadio.html`);
-                }
-            }
-        ]
-    },
-    {
-        label:'FC',
-        submenu:[
-            {
-                label:'Enter to',
-                click(){
-                    mainWindow.loadURL(`file://${__dirname}/index.html`);
-                }
-            }
-        ]
-    },
-    {
-        label:'Options',
-        submenu:[
-            {
-                label:'Update',
-                click(){
-
-                }
-            },
-            {
-                label:'About',
-                click(){
-                    newWin =new BrowserWindow({
-                        width:500,
-                        height:200,
-                        frame:true,//是否显示边缘框
-                        fullscreen:false //是否全屏显示
-                    });
-                    //打开一个新的窗口
-                    // newWin.loadURL(`file://${__dirname}/otherWin.html`);
-                    //新建窗口
-                    newWin.loadURL(`file://${__dirname}/src/html/about.html`);
-                    newWin.on('close',()=>{
-                        newWin=null
-                    });
-                }
-            }
-        ]
-    }
-];
-
 
 app.allowRendererProcessReuse = false
 
@@ -80,15 +24,10 @@ app.on('ready',()=>{
         mode:'bottom'
     });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
-    //mainWindow.loadURL(`file://${__dirname}/liteRadio.html`);
 
     //disable app menu, IF YOU NEED MENU TO DEBUG,UNCOMMENT FOLLOW LINE
     Menu.setApplicationMenu(null);
 
-     //const mainMenu = Menu.buildFromTemplate(menuTemplate);
-     //Menu.setApplicationMenu(mainMenu);
-
-    
     mainWindow.on('closed',()=>{
         mainWindow = null;
 
