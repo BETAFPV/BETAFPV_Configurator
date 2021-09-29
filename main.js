9742f089dad442efad0380b9b40b6c000931af98
wentao <45007731+HWT662988@users.noreply.github.com>
2021-09-29 09:31:01 +0800

    let the welcome page of literadio be the first one loaded

--- main.js
+++ main.js
@@ -23,10 +23,11 @@ app.on('ready',()=>{
     // mainWindow.webContents.openDevTools({
     //     mode:'bottom'
     // });
-    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.loadURL(`file://${__dirname}/LiteRadio.html`);
+    //mainWindow.loadURL(`file://${__dirname}/index.html`);
 
     //disable app menu, IF YOU NEED MENU TO DEBUG,UNCOMMENT FOLLOW LINE
-    //Menu.setApplicationMenu(null);
+    Menu.setApplicationMenu(null);
 
     mainWindow.on('closed',()=>{
         mainWindow = null;
