80a7a3bdc66471f8d70f174f071935cb315d5f1a
LeehotYuu <1472973494@qq.com>
2024-07-08 12:03:54 +0800

    Modify the version number to V2.0-RC3
    更改版本号为V2.0-RC3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const serialport = require('serialport')
 var {shell} = require('electron')
 
-var flightcontrol_configurator_version ='v2.0-RC2';
+var flightcontrol_configurator_version ='v2.0-RC3';
 let isFlasherTab=0;
 var lastPortCount = 0;
 
--- indexLiteRadio.js
+++ indexLiteRadio.js
@@ -1,6 +1,6 @@
 const serialport = require('serialport');
 const semver = require('semver');
-var liteRadio_configurator_version ="v2.0-RC2";
+var liteRadio_configurator_version ="v2.0-RC3";
 var {shell} = require('electron');
 var HID = require('node-hid');
 
